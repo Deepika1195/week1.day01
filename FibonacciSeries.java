3824b2bae13c12369e3e7feaf5c892b22092a3d2
Deepika1195 <90104367+Deepika1195@users.noreply.github.com>
2021-09-04 23:00:49 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+package Week1.day1.assignment;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int a=0,b=1,sum;
+	
+	for(int i =1;i<=8;i++) {
+		System.out.println(a+ "");
+		sum= a+b;
+		a=b;
+		b=sum;
+		
+			
+		}
+
+	}
+
+}
