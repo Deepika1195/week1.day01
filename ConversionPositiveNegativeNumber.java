4f60ec8388c7d32a66fa980369d4c71e7cde8188
Deepika1195 <90104367+Deepika1195@users.noreply.github.com>
2021-09-04 21:23:50 +0530

    Add files via upload

---
+++ ConversionPositiveNegativeNumber.java
@@ -0,0 +1,15 @@
+package Week1.day1.assignment;
+
+public class ConversionPositiveNegativeNumber {
+
+	public static void main(String[] args) {
+		int a = -40;
+if (a<0) {
+	a= a*-1;
+	System.out.println("The number -40 is converted as 40");
+	}
+
+		}
+	}
+
+
---
+++ PositiveOrNegativeNumber.java
@@ -0,0 +1,16 @@
+package Week1.day1.assignment;
+
+public class PositiveOrNegativeNumber {
+
+	public static void main(String[] args) {
+		int i = 35;
+		if(i>0)
+		
+System.out.println("The number is positive");
+else if(i<0)
+
+	System.out.println("The number is Negative");
+
+	}
+
+}
