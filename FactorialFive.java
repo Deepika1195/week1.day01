dc427642c8c47fcf78d28ade8bb25e3948a329c2
Deepika1195 <90104367+Deepika1195@users.noreply.github.com>
2021-09-04 22:09:45 +0530

    Add files via upload

---
+++ FactorialFive.java
@@ -0,0 +1,17 @@
+package Week1.day1.assignment;
+
+public class FactorialFive {
+
+	public static void main(String[] args) {
+int no = 5;
+int fact = 1;
+int i;
+int number = 5;
+for(i =1;i<=no;i++) {
+	fact = fact*i;
+}
+System.out.println("Factor of 5 is " + fact);
+
+	}
+
+}
